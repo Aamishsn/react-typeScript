f300fa7db7cbfe3e114cc6f775389b01ab16e9aa
Aamishsn <aamishsn@gmail.com>
2024-11-08 17:55:04 +0500

    starting-project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,3 @@
- 
-import './App.css'
-
-function App() {
- 
-
-  return (
-    <>
-      amish
-    </>
-  )
+export default function App() {
+  return <h1>Let's get started!</h1>;
 }
-
-export default App
